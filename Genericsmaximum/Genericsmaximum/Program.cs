04574f270e919309abcea2112e23bfa6a52559a3
Dipak@1432 <chavandipakr17@gmail.com>
2022-02-02 00:08:10 +0530

    max number using sort

--- Genericsmaximum/Genericsmaximum/Program.cs
+++ Genericsmaximum/Genericsmaximum/Program.cs
@@ -11,7 +11,7 @@ namespace Genericsmaximum
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to generic demo");
-
+            /*
             Console.WriteLine("Find max Number");
             int output = MaximumNumberCheck.MaximumIntegerNumber(23, 43, 65);
             Console.WriteLine(output);
@@ -26,6 +26,17 @@ namespace Genericsmaximum
             Console.WriteLine(maximum1);
             Console.WriteLine(maximum2);
             Console.WriteLine(maximum3);
+            */
+            Console.WriteLine("Finding max Number :");
+
+            string maximum1 = MaximumNumberCheck.FindMaximum<int>(29, 76, 94);
+            string maximum2 = MaximumNumberCheck.FindMaximum<float>(231.2f, 112.45f, 32.56f);
+            string maximum3 = MaximumNumberCheck.FindMaximum<string>("202", "323", "441");
+
+            Console.WriteLine(maximum1);
+            Console.WriteLine(maximum2);
+            Console.WriteLine(maximum3);
+
 
             Console.ReadLine();
         }
