13c1512fd1a2e672916414416b1f81ac6933681c
Dipak@1432 <chavandipakr17@gmail.com>
2022-02-02 00:03:14 +0530

    string max number is

--- Genericsmaximum/Genericsmaximum/Program.cs
+++ Genericsmaximum/Genericsmaximum/Program.cs
@@ -17,6 +17,16 @@ namespace Genericsmaximum
             Console.WriteLine(output);
             double doubleoutput = MaximumNumberCheck.MaximumFloatNumber(111.2, 22.45, 78.56);
             Console.WriteLine(doubleoutput);
+            Console.WriteLine("Finding max Number :");
+
+            string maximum1 = MaximumNumberCheck.FindMaximum<int>(29, 76, 94);
+            string maximum2 = MaximumNumberCheck.FindMaximum<float>(231.2f, 112.45f, 32.56f);
+            string maximum3 = MaximumNumberCheck.FindMaximum<string>("202", "323", "441");
+
+            Console.WriteLine(maximum1);
+            Console.WriteLine(maximum2);
+            Console.WriteLine(maximum3);
+
             Console.ReadLine();
         }
     }
