7c62f0b60fde7f99c19fa6a07057945b82d54651
Dipak@1432 <chavandipakr17@gmail.com>
2022-02-01 23:51:29 +0530

    float max number is

--- Genericsmaximum/Genericsmaximum/Program.cs
+++ Genericsmaximum/Genericsmaximum/Program.cs
@@ -15,6 +15,8 @@ namespace Genericsmaximum
             Console.WriteLine("Find max Number");
             int output = MaximumNumberCheck.MaximumIntegerNumber(23, 43, 65);
             Console.WriteLine(output);
+            double doubleoutput = MaximumNumberCheck.MaximumFloatNumber(111.2, 22.45, 78.56);
+            Console.WriteLine(doubleoutput);
             Console.ReadLine();
         }
     }
