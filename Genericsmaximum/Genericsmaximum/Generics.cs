6fbe100b694a0a590a4c5f70710e2fcbc743346c
Dipak@1432 <chavandipakr17@gmail.com>
2022-02-02 15:35:18 +0530

    refactor all three to one genric method

---
+++ Genericsmaximum/Genericsmaximum/Generics.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Genericsmaximum
+{
+    internal class Generic<T> where T : IComparable
+    {
+        public void FindMax(T first, T second, T third)
+        {
+            if ((first.CompareTo(second) > 0) && (first.CompareTo(third) > 0))
+            {
+                Console.WriteLine(first + " is greates");
+            }
+            else if ((second.CompareTo(first) > 0) && (second.CompareTo(third) > 0))
+            {
+                Console.WriteLine(second + " is greatest");
+            }
+            else if ((third.CompareTo(second) > 0) && (third.CompareTo(first) > 0))
+            {
+                Console.WriteLine(third + " is greatest");
+            }
+            else
+            {
+                Console.WriteLine("All are equal");
+
+            }
+        }
+    }
+}
--- Genericsmaximum/Genericsmaximum/Program.cs
+++ Genericsmaximum/Genericsmaximum/Program.cs
@@ -38,16 +38,9 @@ namespace Genericsmaximum
              Console.WriteLine(maximum3);
             */
             Console.WriteLine("Finding max Number :");
+            Generic<int> first = new Generic<int>();
+            first.FindMax(1, 2, 3);
 
-            int[] intArray = { 242, 112, 912, 220, 178 };
-            GenericMaximum<int> generic = new GenericMaximum<int>(intArray);
-            generic.PrintMaxValue();
-            double[] doubleArray = { 21.2, 34.4, 14.32, 25.5, 16.78 };
-            GenericMaximum<double> genericDouble = new GenericMaximum<double>(doubleArray);
-            genericDouble.PrintMaxValue();
-            string[] stringArray = { "101", "422", "233", "552", "109" };
-            GenericMaximum<string> genericString = new GenericMaximum<string>(stringArray);
-            genericString.PrintMaxValue();
 
 
 
